fb0916fdfb494d4649d623887581ad9eed817ce5
Yagiz Bayrak <yagizbayrak01@gmail.com>
2026-08-20 11:08:33 +0100

    Read instruction and data memory asynchronously

--- rtl/memory/data_mem.v
+++ rtl/memory/data_mem.v
@@ -1,3 +1,4 @@
+// Data memory with asynchronous reads and synchronous writes for the memory stage.
 module data_mem #(
     parameter MEMORY_LENGTH = 1024
 )
@@ -7,20 +8,16 @@ module data_mem #(
     input [31:0] i_data,
     input mem_write,
     input mem_read,
-    output reg [31:0] o_data
+    output [31:0] o_data
 );
 
     reg [31:0] mem [0:MEMORY_LENGTH - 1];
 
     always @(posedge clk) begin
-        if (mem_read & !mem_write)
-            o_data <= mem[addr];
-        else if (!mem_read & mem_write)
+        if (mem_write)
             mem[addr] <= i_data;
-        else
-            ;
-
     end
+    assign o_data = mem_read ? mem[addr] : 32'd0;
 
 endmodule
     
