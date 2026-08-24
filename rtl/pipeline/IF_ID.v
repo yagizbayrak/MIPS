891a15b9375ef321a3352cc0b289a6f0ee917afb
Yagiz Bayrak <yagizbayrak01@gmail.com>
2026-08-24 10:56:59 +0100

    Rework IF_ID and MEM_WB for SRAM timing

--- rtl/pipeline/IF_ID.v
+++ rtl/pipeline/IF_ID.v
@@ -5,18 +5,17 @@ module IF_ID (
     input  wire        stall,
     input  wire        flush,
     input  wire [31:0] i_pc_plus4,
-    input  wire [31:0] i_instruction,
     output reg  [31:0] o_pc_plus4,
-    output reg  [31:0] o_instruction
+    output reg         o_squash
 );
 always @(posedge clk) begin
     if (reset | flush) begin
-        o_pc_plus4    <= 32'd0;
-        o_instruction <= 32'd0;
+        o_pc_plus4 <= 32'd0;
+        o_squash   <= 1'b1;
     end
     else if (!stall) begin
-        o_pc_plus4    <= i_pc_plus4;
-        o_instruction <= i_instruction;
+        o_pc_plus4 <= i_pc_plus4;
+        o_squash   <= 1'b0;
     end
 end
 endmodule
